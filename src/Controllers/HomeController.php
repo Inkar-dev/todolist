7fd9aa24df0c7ba7697310e447d4b25f8a8b8458
Инкар Жоламанова <inkarmusa9@gmail.com>
2024-08-27 20:18:05 +0600

    change method with call method render

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,13 +1,14 @@
 <?php
 namespace App\Controllers;
 use App\Views\Home;
-
+use App\Models\Task;
 class HomeController
 {
     public function index()
     {
+        $task = new Task();
+        $tasks  = $task->getTasks();
         $view = new Home();
-        return $view->index();
+        $view->render($tasks);
     }
-
 }
